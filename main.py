ec5ed07d28a76986f6a963384f04123b401b5b00
Liaorihong <148600924+Liaorihong@users.noreply.github.com>
2025-12-25 13:55:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 Hi 
 This is the original code for my first SCI about CeA Crh neurons and gut microbios.
-Now I am a third year master student in Jiangnan University majoring in Neuroscience. My primary interests focuses on mechanosensory. But due to some interesting reasons
-,maybe I cannot see my longterm work published.
+Now I am a third year master student in Jiangnan University majoring in Neuroscience. My primary interests focuses on mechanosensory. 
+
+
